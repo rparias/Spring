5e1fcf0cb413a2c5529926ad1b30de3a05d4ecf9
ronald.arias <ronald.arias@udla.edu.ec>
2018-02-20 19:40:04 -0500

    Prueba simple de conexion con hibernate

---
+++ hibernate-tutorial/src/com/ronaldarias/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.ronaldarias.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			
+			System.out.println("Conecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Conexion exitosa!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
