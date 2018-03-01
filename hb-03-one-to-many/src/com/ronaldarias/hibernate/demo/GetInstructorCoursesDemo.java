bf91ba291233055d96d93e40f2adaeee15848532
ronald.arias <ronald.arias@udla.edu.ec>
2018-03-01 16:32:27 -0500

    Obtener los cursos de un instructor @OneToMany

---
+++ hb-03-one-to-many/src/com/ronaldarias/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,48 @@
+package com.ronaldarias.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ronaldarias.demo.entity.Course;
+import com.ronaldarias.demo.entity.Instructor;
+import com.ronaldarias.demo.entity.InstructorDetail;
+
+public class GetInstructorCoursesDemo {
+
+	public static void main(String[] args) {
+
+		//crear session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)	//se añade el curso para esta clase
+									.buildSessionFactory();
+		
+		//crear session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//iniciar la transaccion
+			session.beginTransaction();
+			
+			//obtener el instructor de la base de datos
+			int theID = 1;
+			Instructor instructor = session.get(Instructor.class, theID);
+			System.out.println("Instructor: " + instructor);
+			
+			System.out.println("COURSES: " + instructor.getCourses());
+			
+			//realizar un commit de la transaccion
+			session.getTransaction().commit();
+			System.out.println("Transaccion realizada!");
+			
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
