a78c2b52bf97bba040b61a6d2f53199e2840e95a
ronald.arias <ronald.arias@udla.edu.ec>
2018-03-02 17:20:46 -0500

    @OneToMany unidireccional - Borrar. Si se elimina un curso se eliminan todos los reviews de ese curso.

---
+++ hb-04-one-to-many-uni/src/com/ronaldarias/hibernate/demo/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,53 @@
+package com.ronaldarias.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ronaldarias.demo.entity.Course;
+import com.ronaldarias.demo.entity.Instructor;
+import com.ronaldarias.demo.entity.InstructorDetail;
+import com.ronaldarias.demo.entity.Review;
+
+public class DeleteCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		//crear session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)	
+									.addAnnotatedClass(Review.class)	//se añade review para esta clase
+									.buildSessionFactory();
+		
+		//crear session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//iniciar la transaccion
+			session.beginTransaction();
+			
+			//obtener el curso
+			int theId = 10;
+			Course course = session.get(Course.class, theId);
+			//mostrar los reviews de ese curso
+			System.out.println("Curso: " + course);
+			System.out.println("Reviews: " + course.getReviews());
+			
+			//borrar el curso
+			session.delete(course);
+			
+			//realizar un commit de la transaccion
+			session.getTransaction().commit();
+			System.out.println("Transaccion realizada!");
+			
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
