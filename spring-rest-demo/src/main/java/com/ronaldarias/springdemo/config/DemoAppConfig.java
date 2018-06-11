c1a3e137f0c84d93c9de9059de045066f125f314
Ronald Arias <ronald.arias@udla.edu.ec>
2018-06-11 17:35:23 -0500

    Configuracion inicial para servicios rest

---
+++ spring-rest-demo/src/main/java/com/ronaldarias/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.ronaldarias.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.ronaldarias.springdemo")
+public class DemoAppConfig {
+
+}
