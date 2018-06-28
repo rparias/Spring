9a0dda21a613befd2f79840ace9568930c3128ee
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-28 16:56:47 -0500

    Servicio POST para agregar un customer

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,22 @@ public class CustomerRestController {
 		}
 		
 		return customer;
-	} 
+	}
+	
+	
+	// add mapping for POST /customers - add new customer
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
+		
+		customer.setId(0);
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+		
+	}
 }
